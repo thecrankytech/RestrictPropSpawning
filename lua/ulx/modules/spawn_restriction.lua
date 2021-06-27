49a79c27f5e7cb1af52a7864a35b04937d9ef63a
Maximilian Grüttemeier <flisim2004@outlook.de>
2021-06-27 23:47:58 +0200

    Make permission readable
    Make permissions more readable in the group access management

--- lua/ulx/modules/spawn_restriction.lua
+++ lua/ulx/modules/spawn_restriction.lua
@@ -10,13 +10,13 @@ ULib.registerPlugin({
 -- Permission check hook: 
 hook.Add("PlayerSpawnProp", "sr_RestrictPropSpawning", function(ply, model) -- add uniqueness to unique hook id
     -- check if user has permissions
-    if not ply:query( 'CanSpawnProps' ) then
+    if not ply:query( 'Can Spawn Props' ) then
         -- Print error to users chat and console
         ULib.tsayError(ply, 'You don\'t have permissions to spawn props')
         return false
     end -- I don't think it's necessary to notify the user if something has been spawned successfully, another hook may still deny it
 end)
-ULib.ucl.registerAccess('CanSpawnProps', 'user', 'Allows user to spawn props', 'KNIGHTs SpawnRestriction')
+ULib.ucl.registerAccess('Can Spawn Props', 'user', 'Allows user to spawn props', 'KNIGHTs SpawnRestriction')
 
 --[[
     NPC spawning restriction
@@ -24,13 +24,13 @@ ULib.ucl.registerAccess('CanSpawnProps', 'user', 'Allows user to spawn props', '
 -- Permission check hook: 
 hook.Add("PlayerSpawnNPC", "sr_RestrictNPCSpawning", function(ply, npcType, wep) -- add uniqueness to unique hook id
     -- check if user has permissions
-    if not ply:query( 'CanSpawnNPCs' ) then
+    if not ply:query( 'Can Spawn NPCs' ) then
         -- Print error to users chat and console
         ULib.tsayError(ply, 'You don\'t have permissions to spawn NPCs')
         return false
     end -- I don't think it's necessary to notify the user if something has been spawned successfully, another hook may still deny it
 end)
-ULib.ucl.registerAccess('CanSpawnNPCs', 'user', 'Allows user to spawn props', 'KNIGHTs SpawnRestriction')
+ULib.ucl.registerAccess('Can Spawn NPCs', 'user', 'Allows user to spawn NPCs', 'KNIGHTs SpawnRestriction')
 
 --[[
     SWEP give restriction
@@ -38,13 +38,13 @@ ULib.ucl.registerAccess('CanSpawnNPCs', 'user', 'Allows user to spawn props', 'K
 -- Permission check hook: 
 hook.Add("PlayerGiveSWEP", "sr_RestrictWeaponSpawning", function(ply, weapon, swep) -- add uniqueness to unique hook id
     -- check if user has permissions
-    if not ply:query( 'CanGiveSWEPs' ) then
+    if not ply:query( 'Can Give SWEPs' ) then
         -- Print error to users chat and console
         ULib.tsayError(ply, 'You don\'t have permissions to give SWEPs')
         return false
     end -- I don't think it's necessary to notify the user if something has been spawned successfully, another hook may still deny it
 end)
-ULib.ucl.registerAccess('CanGiveSWEPs', 'user', 'Allows user to spawn props', 'KNIGHTs SpawnRestriction')
+ULib.ucl.registerAccess('Can Give SWEPs', 'user', 'Allows user to give SWEPs', 'KNIGHTs SpawnRestriction')
 
 --[[
     SWEP spawn restriction
@@ -52,10 +52,10 @@ ULib.ucl.registerAccess('CanGiveSWEPs', 'user', 'Allows user to spawn props', 'K
 -- Permission check hook: 
 hook.Add("PlayerSpawnSWEP", "sr_RestrictWeaponSpawning", function(ply, weapon, swep) -- add uniqueness to unique hook id
     -- check if user has permissions
-    if not ply:query( 'CanSpawnSWEPs' ) then
+    if not ply:query( 'Can Spawn SWEPs' ) then
         -- Print error to users chat and console
         ULib.tsayError(ply, 'You don\'t have permissions to spawn SWEPs')
         return false
     end -- I don't think it's necessary to notify the user if something has been spawned successfully, another hook may still deny it
 end)
-ULib.ucl.registerAccess('CanSpawnSWEPs', 'user', 'Allows user to spawn props', 'KNIGHTs SpawnRestriction')
+ULib.ucl.registerAccess('Can Spawn SWEPs', 'user', 'Allows user to spawn SWEPs', 'KNIGHTs SpawnRestriction')
