66a301ed93b5bac3c315b181454a0ccce191e947
Knight <68920705+therealknight@users.noreply.github.com>
2021-06-27 19:26:25 +0100

    Add weapon spawning restriction

--- restrictpropspawning.lua
+++ restrictpropspawning.lua
@@ -1,5 +1,6 @@
 local PropErrorMsg = "[ERROR] Spawning props is restricted to staff only."
 local NPCErrorMsg = "[ERROR] Spawning NPCs is restricted to staff only."
+local WeaponErrorMsg = "[ERROR] Spawning NPCs is restricted to staff only."
 local staff = {
     ["superadmin"] = true,
     ["admin"] = true,
@@ -21,5 +22,23 @@ hook.Add("PlayerSpawnNPC", "RestrictNPCSpawning", function(ply, npcType, wep)
         return false
     else
         ply:ChatPrint("Successfully spawned: " .. npcType)
+    end
+end)
+
+hook.Add("PlayerGiveSWEP", "RestrictWeaponSpawning", function(ply, weapon, swep)
+    if not staff[ply:GetNWString("usergroup")] then
+        ply:ChatPrint( WeaponErrorMsg )
+        return false
+    else
+        ply:ChatPrint("Successfully spawned: " .. weapon)
+    end
+end)
+
+hook.Add("PlayerSpawnSWEP", "RestrictWeaponSpawning", function(ply, weapon, swep)
+    if not staff[ply:GetNWString("usergroup")] then
+        ply:ChatPrint( WeaponErrorMsg )
+        return false
+    else
+        ply:ChatPrint("Successfully spawned: " .. weapon)
     end
 end)
