b236f145239a86ad388829efc69b9b1bc184f9ed
Maximilian Grüttemeier <flisim2004@outlook.de>
2021-06-27 23:32:44 +0200

    Permission Check
    Replace lua group table with a permission check to make the addon more accessible. Also switch chat error messages to tsay and remove success message.

--- lua/ulx/modules/spawn_restriction.lua
+++ lua/ulx/modules/spawn_restriction.lua
@@ -1,44 +1,61 @@
-local PropErrorMsg = "[ERROR] Spawning props is restricted to staff only."
-local NPCErrorMsg = "[ERROR] Spawning NPCs is restricted to staff only."
-local WeaponErrorMsg = "[ERROR] Spawning NPCs is restricted to staff only."
-local staff = {
-    ["superadmin"] = true,
-    ["admin"] = true,
-    ["operator"] = true,
-}
-
-hook.Add("PlayerSpawnProp", "RestrictPropSpawning", function(ply, model)
-    if not staff[ply:GetNWString("usergroup")] then
-        ply:ChatPrint( PropErrorMsg )
+ULib.registerPlugin({
+    Name = 'therealknight\'s Spawn restriction',
+    Version = '1.0.0',
+    Author ='therealknight',
+    URL = 'https://github.com/therealknight/RestrictPropSpawning'
+})
+--[[
+    Prop spawning restriction
+]]
+-- Permission check hook: 
+hook.Add("PlayerSpawnProp", "sr_RestrictPropSpawning", function(ply, model) -- add uniqueness to unique hook id
+    -- check if user has permissions
+    if not ply:query( 'CanSpawnProps' ) then
+        -- Print error to users chat and console
+        ULib.tsayError(ply, 'You don\'t have permissions to spawn props')
         return false
-    else
-        ply:ChatPrint("Successfully spawned: " .. model)
-    end
+    end -- I don't think it's necessary to notify the user if something has been spawned successfully, another hook may still deny it
 end)
+ULib.ucl.registerAccess('CanSpawnProps', 'user', 'Allows user to spawn props', 'KNIGHTs SpawnRestriction')
 
-hook.Add("PlayerSpawnNPC", "RestrictNPCSpawning", function(ply, npcType, wep)
-    if not staff[ply:GetNWString("usergroup")] then
-        ply:ChatPrint( NPCErrorMsg )
+--[[
+    NPC spawning restriction
+]]
+-- Permission check hook: 
+hook.Add("PlayerSpawnNPC", "sr_RestrictNPCSpawning", function(ply, npcType, wep) -- add uniqueness to unique hook id
+    -- check if user has permissions
+    if not ply:query( 'CanSpawnNPCs' ) then
+        -- Print error to users chat and console
+        ULib.tsayError(ply, 'You don\'t have permissions to spawn NPCs')
         return false
-    else
-        ply:ChatPrint("Successfully spawned: " .. npcType)
-    end
+    end -- I don't think it's necessary to notify the user if something has been spawned successfully, another hook may still deny it
 end)
+ULib.ucl.registerAccess('CanSpawnNPCs', 'user', 'Allows user to spawn props', 'KNIGHTs SpawnRestriction')
 
-hook.Add("PlayerGiveSWEP", "RestrictWeaponSpawning", function(ply, weapon, swep)
-    if not staff[ply:GetNWString("usergroup")] then
-        ply:ChatPrint( WeaponErrorMsg )
+--[[
+    SWEP give restriction
+]]
+-- Permission check hook: 
+hook.Add("PlayerGiveSWEP", "sr_RestrictWeaponSpawning", function(ply, weapon, swep) -- add uniqueness to unique hook id
+    -- check if user has permissions
+    if not ply:query( 'CanGiveSWEPs' ) then
+        -- Print error to users chat and console
+        ULib.tsayError(ply, 'You don\'t have permissions to give SWEPs')
         return false
-    else
-        ply:ChatPrint("Successfully spawned: " .. weapon)
-    end
+    end -- I don't think it's necessary to notify the user if something has been spawned successfully, another hook may still deny it
 end)
+ULib.ucl.registerAccess('CanGiveSWEPs', 'user', 'Allows user to spawn props', 'KNIGHTs SpawnRestriction')
 
-hook.Add("PlayerSpawnSWEP", "RestrictWeaponSpawning", function(ply, weapon, swep)
-    if not staff[ply:GetNWString("usergroup")] then
-        ply:ChatPrint( WeaponErrorMsg )
+--[[
+    SWEP spawn restriction
+]]
+-- Permission check hook: 
+hook.Add("PlayerSpawnSWEP", "sr_RestrictWeaponSpawning", function(ply, weapon, swep) -- add uniqueness to unique hook id
+    -- check if user has permissions
+    if not ply:query( 'CanSpawnSWEPs' ) then
+        -- Print error to users chat and console
+        ULib.tsayError(ply, 'You don\'t have permissions to spawn SWEPs')
         return false
-    else
-        ply:ChatPrint("Successfully spawned: " .. weapon)
-    end
+    end -- I don't think it's necessary to notify the user if something has been spawned successfully, another hook may still deny it
 end)
+ULib.ucl.registerAccess('CanSpawnSWEPs', 'user', 'Allows user to spawn props', 'KNIGHTs SpawnRestriction')
